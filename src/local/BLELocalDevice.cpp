026b6e72599e5b39886e6ffde7020dec091c9d08
Jeremy Williams <alwayslinux2@yahoo.com>
2022-06-15 19:24:32 -0400

    Fixing define name that should have referred to board define from boards.txt

--- src/local/BLELocalDevice.cpp
+++ src/local/BLELocalDevice.cpp
@@ -46,7 +46,7 @@ BLELocalDevice::~BLELocalDevice()
 
 int BLELocalDevice::begin()
 {
-#if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_AVR_UNO_WIFI_REV2) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_NANO_RP2040_CONNECT) || defined(ARDUINO_METRO_M4_AIRLIFT_LITE)
+#if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_AVR_UNO_WIFI_REV2) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_NANO_RP2040_CONNECT) || defined(ADAFRUIT_METRO_M4_AIRLIFT_LITE)
   // reset the NINA in BLE mode
   pinMode(SPIWIFI_SS, OUTPUT);
   pinMode(NINA_RESETN, OUTPUT);
@@ -61,7 +61,7 @@ int BLELocalDevice::begin()
   delay(100);
   digitalWrite(NINA_RESETN, LOW);
   delay(750);
-#elif defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_NANO_RP2040_CONNECT) || defined(ARDUINO_METRO_M4_AIRLIFT_LITE)
+#elif defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_NANO_RP2040_CONNECT) || defined(ADAFRUIT_METRO_M4_AIRLIFT_LITE)
   // inverted reset
   digitalWrite(NINA_RESETN, LOW);
   delay(100);
@@ -74,7 +74,7 @@ int BLELocalDevice::begin()
 #endif
 
 
-#if defined(ARDUINO_AVR_UNO_WIFI_REV2) || defined(ARDUINO_METRO_M4_AIRLIFT_LITE)
+#if defined(ARDUINO_AVR_UNO_WIFI_REV2) || defined(ADAFRUIT_METRO_M4_AIRLIFT_LITE)
   // set SS HIGH
   digitalWrite(SPIWIFI_SS, HIGH);
 
@@ -195,11 +195,7 @@ void BLELocalDevice::end()
 #if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
   // disable the NINA
   digitalWrite(NINA_RESETN, HIGH);
-<<<<<<< HEAD
-#elif defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_METRO_M4_AIRLIFT_LITE)
-=======
-#elif defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_NANO_RP2040_CONNECT)
->>>>>>> 9bf21b767bbc380f69dbfad7168dcc4f469c93b6
+#elif defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_NANO_RP2040_CONNECT) || defined(ADAFRUIT_METRO_M4_AIRLIFT_LITE)
   // disable the NINA
   digitalWrite(NINA_RESETN, LOW);
 #elif defined(ARDUINO_PORTENTA_H7_M4) || defined(ARDUINO_PORTENTA_H7_M7) || defined(ARDUINO_NICLA_VISION)
--- src/utility/HCIUartTransport.cpp
+++ src/utility/HCIUartTransport.cpp
@@ -84,7 +84,7 @@ int HCIUartTransportClass::read()
 
 size_t HCIUartTransportClass::write(const uint8_t* data, size_t length)
 {
-#if defined(ARDUINO_AVR_UNO_WIFI_REV2) || defined(ARDUINO_METRO_M4_AIRLIFT_LITE)
+#if defined(ARDUINO_AVR_UNO_WIFI_REV2) || defined(ADAFRUIT_METRO_M4_AIRLIFT_LITE)
   // wait while the CTS pin is low
   while (digitalRead(NINA_CTS) == HIGH);
 #endif
@@ -98,7 +98,7 @@ size_t HCIUartTransportClass::write(const uint8_t* data, size_t length)
 
 #if defined(ARDUINO_AVR_UNO_WIFI_REV2) || defined(ARDUINO_NANO_RP2040_CONNECT)
 HCIUartTransportClass HCIUartTransport(SerialHCI, 119600);
-#elif defined(ADAFRUIT_ITSYBITSY_M4_EXPRESS) || defined(ARDUINO_METRO_M4_AIRLIFT_LITE)
+#elif defined(ADAFRUIT_ITSYBITSY_M4_EXPRESS) || defined(ADAFRUIT_METRO_M4_AIRLIFT_LITE)
 HCIUartTransportClass HCIUartTransport(SerialHCI, 115200);
 #else
 HCIUartTransportClass HCIUartTransport(SerialHCI, 912600);
